559b6ca37d5125ac7b39cfdb139034510db2da15
Beverly Appleton <bfcain19@gmail.com>
2020-04-14 03:18:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,14 +32,8 @@ class Song
     genre_count
   end
   def self.artist_count
-    artist_count {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
+    artist_count = Hash.new(0)
+    @@artists.each {|artist| artist_count[artist] += 1}
     artist_count
   end
 end

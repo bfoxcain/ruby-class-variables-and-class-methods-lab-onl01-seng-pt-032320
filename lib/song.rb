5d871a8d9e077eb68acdae6742533aa4cc01d938
Beverly Appleton <bfcain19@gmail.com>
2020-04-14 03:09:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   @@genres = []
   @@artists = []
 
-  def initialize(song_name, artist, genre)
+  def initialize(name, artist, genre)
   @name = song_name
   @artists = artists
   @genre = genre
